8b0acf25f0f83a0b25e8f050efd34d4f089e03b7
SergeyGarinV <SergeyGarinV>
2020-12-09 22:44:49 +0300

    Commit Lesson 3

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main.views import main_view
 from todo_item.views import item_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_view),
-    path('item/', item_view)
+    path('', include('main.urls')),
+    path('item/', include('todo_item.urls'))
 ]
